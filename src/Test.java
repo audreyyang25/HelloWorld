7e905a634b7637ded3bb675d451b5d8194617b58
Audrey Yang <audreytyang@gmail.com>
2022-08-26 08:44:34 -0700

    Prints day of the week

--- src/Test.java
+++ src/Test.java
@@ -2,8 +2,8 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.print("Hello World!");
-
+		System.out.println("Hello World!");
+		System.out.println ("Today is Friday!");
 	}
 
 }
